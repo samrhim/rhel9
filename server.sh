410558a03ced5a6b1cfc9d261e86af46007a414c
samrhim <rhimi1111@gmail.com>
2023-12-22 20:37:55 +0100

    verify file

--- server.sh
+++ server.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
 echo "bootstrapping rhel9 server"
-
 # Setting and configuring the VM
 
 dnf update -y
@@ -10,4 +8,11 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_
 systemctl restart sshd
 PASS=$(echo "rhel9" | openssl passwd -1 -stdin)
 useradd -p  "$PASS" -s /bin/bash -d /home/student -m  student
-echo "student  ALL=(ALL:ALL) NOPASSWD: ALL"  >> /etc/sudoers.d/student
+
+file="/etc/sudoers.d/student"
+ Check if the file exists
+if [ -e "$file" ]; then
+    echo "The file already exists."
+else
+    echo "student  ALL=(ALL:ALL) NOPASSWD: ALL"  >> /etc/sudoers.d/student
+fi
